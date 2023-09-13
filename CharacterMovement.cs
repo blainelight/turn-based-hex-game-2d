cd0cd0887b94d2528d29867127d8456066b9db31
blainelight <40840394+blainelight@users.noreply.github.com>
2023-09-12 23:01:21 -0700

    boomski

--- CharacterMovement.cs
+++ CharacterMovement.cs
@@ -8,6 +8,8 @@ public class CharacterMovement : MonoBehaviour
     public LayerMask layerMask;
     public float threshold = 0.5f;
 
+    private FlashFeedback flashFeedback;
+
     private GameObject selectedObject;
 
     // Update is called once per frame
@@ -25,16 +27,22 @@ public class CharacterMovement : MonoBehaviour
     }
      private void HandleSelection()
     {
-        Vector3 mouseInput = currentCamera.ScreenToWorldPoint(Input.mousePosition);
-        mouseInput.z = 0f;
-        Collider2D collider = Physics2D.OverlapPoint(mouseInput, layerMask);
+        Vector3 mouseInput = currentCamera.ScreenToWorldPoint(Input.mousePosition); //This takes the mouse's position on the screen and converts it into a point in the game world.
+        mouseInput.z = 0f; //This sets the z-coordinate of that point to 0, effectively making sure we're working in a 2D plane.
+        Collider2D collider = Physics2D.OverlapPoint(mouseInput, layerMask); //This checks if there's any 2D object (with a Collider2D component) at the point where the mouse clicked, considering only objects on layers specified by layerMask. Here, the layermask is Agent, and the object clicked is the FarmerObject with a 2D colider set on it. 
         selectedObject = collider == null ? null : collider.gameObject; //collider can be null or referencing the agent. if colider = null, it will be null, else get collider.getobject.
+
+        if(selectedObject!= null)
+            flashFeedback = selectedObject.GetComponent<FlashFeedback>(); //this grabs the FlashFeedback compentent from the selectedObject
+            flashFeedback.PlayFeedback(); //this calls the PlayFeedback method we wrote which will make the thing flash
     }
 
     private void HandleMovement() //drag mouse and let go of button//determine if we can select object or not
     {
         if(selectedObject == null)
             return;
+        
+        flashFeedback.StopFeedback();
         
         Vector3 endPosition = currentCamera.ScreenToWorldPoint(Input.mousePosition);
         endPosition.z = 0f; //for staftey of calc correct position
@@ -45,7 +53,7 @@ public class CharacterMovement : MonoBehaviour
             
             if (Mathf.Abs(direction.x)> Mathf.Abs(direction.y)) //if abs value of x > abs of y, this means we will move up or down, left or right depending on which is greater
             {
-                float sign = Mathf.Sign(direction.x);
+                float sign = Mathf.Sign(direction.x); // turns the direction that the mouse was released into -1, 0, or +1
                 direction = Vector2.right * sign;
             }
             else
@@ -53,7 +61,7 @@ public class CharacterMovement : MonoBehaviour
                 float sign = Mathf.Sign(direction.y);
                 direction = Vector2.up * sign;
             }
-            selectedObject.transform.position += (Vector3)direction;
+            selectedObject.transform.position += (Vector3)direction; //casts director as a Vector3 to make it compatible with the transform and I think unity is 3D by default so maybe gameobjets have to be 3D. 
         }
     }
 }
---
+++ Feedback/FlashFeedback.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashFeedback : MonoBehaviour
+{
+
+    public SpriteRenderer spriteRenderer; //to set the sprite to be either visible or invisible, to make it flash
+    [SerializeField] //this pertains to the thing below
+    private float invisibleTime, visibleTime; //times controlling anamation feedback
+
+    public void PlayFeedback()
+    {
+        if(spriteRenderer == null )
+            return;
+        StopFeedback();
+        StartCoroutine(FlashCoroutine());
+    }
+
+    private IEnumerator FlashCoroutine()
+    {
+        Color spriteColor = spriteRenderer.color; //usually white
+        spriteColor.a = 0; //alpha, transparency - 0 means invisible
+        spriteRenderer.color = spriteColor; // again, set the sprite renderer to the color - we do this again because color is a value type - we cannot modify it on our sprite rendered; we need to assign a new one, but with the alpha = 0 this time. 
+        yield return new WaitForSeconds(invisibleTime);
+        
+        spriteColor.a = 1; //alpha, transparency - 1 means normal visible
+        spriteRenderer.color = spriteColor; // again, set the sprite renderer to the color - we do this again because color is a value type - we cannot modify it on our sprite rendered; we need to assign a new one, but with the alpha = 0 this time. 
+        yield return new WaitForSeconds(visibleTime);
+
+        StartCoroutine(FlashCoroutine);
+    }
+
+    public void StopFeedback()
+    {
+        StopAllCoroutines(); //A Couroutine is a process that hands back control to the unity engine then comes back -- useful for animations
+        Color spriteColor = spriteRenderer.color;
+        spriteColor.a = 1; 
+        spriteRenderer.color = spriteColor; 
+    }
+
+}
